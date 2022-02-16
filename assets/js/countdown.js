210ee1d9b44a285e3cb280194157191965eb6a11
Jay Colson <jay@karma.net>
2022-02-16 19:10:49 +0000

    add vacation countdown!

---
+++ assets/js/countdown.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    future = Date.parse("Mar 21, 2022 10:00:00");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("countdown")
+    .innerHTML =
+    '<div>' + d + '<span>Days</span></div>' +
+    '<div>' + h + '<span>Hours</span></div>' +
+    '<div>' + m + '<span>Minutes</span></div>' +
+    '<div>' + s + '<span>Seconds</span></div>';
+}
+let countdownInt = setInterval('updateCountdown()', 1000);
